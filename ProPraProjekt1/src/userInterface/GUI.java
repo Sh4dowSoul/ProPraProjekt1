ecd26fe00582711c2a9488be815f6a0ec54c2eaf
User <User@Thinkpad.fritz.box>
2017-11-23 16:38:38 +0100

    deutsch, GUIController(ActionListener), implementierung in gui.java , visuelle strukturierung

--- ProPraProjekt1/src/userInterface/GUI.java
+++ ProPraProjekt1/src/userInterface/GUI.java
@@ -1,11 +1,34 @@
 package userInterface;
 
-/**
- * The Class GUI
- * 
- * @author name
- */
+import java.io.IOException;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+public class GUI extends Application {
+	private Stage primaryStage;
+	private AnchorPane mainLayout;
+	
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("DefectManager");
+		showMainView();
+	}
+	
+	private void showMainView() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(GUI.class.getResource("GUI.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
