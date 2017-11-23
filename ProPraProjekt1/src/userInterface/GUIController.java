85b740866592e2b65d66275477d25935bf0ba535
User <User@Thinkpad.fritz.box>
2017-11-23 16:39:21 +0100

    GUIController

---
+++ ProPraProjekt1/src/userInterface/GUIController.java
@@ -0,0 +1,31 @@
+package userInterface;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GUIController {
+	@FXML
+	private Label testLabel;
+	
+	public void add(ActionEvent add) {
+		testLabel.setText("jojo");
+	}
+	
+	public void del(ActionEvent del) {
+		
+	}
+	
+	public void filter(ActionEvent filter) {
+		
+	}
+	
+	public void export(ActionEvent export) {
+		
+	}
+	
+	public void stats(ActionEvent stats) {
+		
+	}
+	
+}
