59601c05665da81b717a601d584ab33bdd638ec2
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2017-11-27 14:37:12 +0100

    Company: Verändere Attribute

--- ProPraProjekt1/src/applicationLogic/Company.java
+++ ProPraProjekt1/src/applicationLogic/Company.java
@@ -8,28 +8,31 @@ package applicationLogic;
 
 public class Company {
 
+	private int id;
 	private String name;
-	private String address;
+	private String hqStreet;
+	private String hqZip;
 	
-	public Company(String name, String address) {
+	public Company(int id, String name, String hqStreet, String hqZip) {
+		this.id = id;
 		this.name = name;
-		this.address = address;
+		this.hqStreet = hqStreet;
+		this.hqZip = hqZip;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getHqStreet() {
+		return hqStreet;
 	}
 
-	public String getAddress() {
-		return address;
+	public String getHqZip() {
+		return hqZip;
 	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
 }
