140d3a3e8964d3dfc9ee7a10bebe9ac9a849fc29
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2017-12-01 17:24:31 +0100

    GUI Controller/Statistik Tab: Zeige Firmen Liste an

--- ProPraProjekt1/src/userInterface/GUIController.java
+++ ProPraProjekt1/src/userInterface/GUIController.java
@@ -4,25 +4,33 @@ import java.awt.List;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import org.controlsfx.control.textfield.AutoCompletionBinding;
 import org.controlsfx.control.textfield.TextFields;
 
+import applicationLogic.Company;
 import applicationLogic.PDFExport;
 import dataStorageAccess.controller.BranchController;
 import dataStorageAccess.controller.DefectController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
 
 public class GUIController implements Initializable{
 	@FXML
 	private Label testLabel;
 	@FXML private TextField defectSearchField;
+	@FXML private ListView<Company> companyList;
 	
 	public void add(ActionEvent add) {
 		testLabel.setText("jojo");
@@ -58,11 +66,29 @@ public class GUIController implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
- 
+		//TODO: Über Threads nachdenken.
         try {
+        	//AutoComplete
         	AutoCompletionBinding ab = TextFields.bindAutoCompletion(defectSearchField, DefectController.getAllDefects() );
         	ab.setMinWidth(600);
         	//ab.setOnAutoCompleted(e -> System.out.println(ab.getOnAutoCompleted())); --> Wenn man etwas ausgewählt hat
+        	
+    		//CompanyList
+    		ObservableList<Company> items = FXCollections.observableArrayList(dataStorageAccess.controller.CompanyController.getCompanies());
+    		companyList.setItems(items);
+    		companyList.setCellFactory(param -> new ListCell<Company>() {
+    			@Override
+    			protected void updateItem(Company item, boolean empty) {
+    				super.updateItem(item, empty);
+    				if (empty || item == null) {
+    					setText(null);
+    					setGraphic(null);
+    				}
+    				else {
+    					setText(item.getName() + " - ID: " + item.getId());
+    				} 
+    			}
+    		});
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
