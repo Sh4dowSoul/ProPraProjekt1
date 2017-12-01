2f1f3510da009b51c2e2511d9b18e6d37e2f1783
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2017-12-01 16:44:17 +0100

    GUIController: Erste Version der AutoVervollständigung

--- ProPraProjekt1/src/userInterface/GUIController.java
+++ ProPraProjekt1/src/userInterface/GUIController.java
@@ -2,16 +2,27 @@ package userInterface;
 
 import java.awt.List;
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
 
 import applicationLogic.PDFExport;
+import dataStorageAccess.controller.BranchController;
+import dataStorageAccess.controller.DefectController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class GUIController {
+public class GUIController implements Initializable{
 	@FXML
 	private Label testLabel;
+	@FXML private TextField defectSearchField;
 	
 	public void add(ActionEvent add) {
 		testLabel.setText("jojo");
@@ -44,5 +55,18 @@ public class GUIController {
 	public void loadPlant(ActionEvent loadPlant) {
 		
 	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+ 
+        try {
+        	AutoCompletionBinding ab = TextFields.bindAutoCompletion(defectSearchField, DefectController.getAllDefects() );
+        	ab.setMinWidth(600);
+        	//ab.setOnAutoCompleted(e -> System.out.println(ab.getOnAutoCompleted())); --> Wenn man etwas ausgewählt hat
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 	
 }
