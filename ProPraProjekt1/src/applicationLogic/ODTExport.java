2dfcff0a0efa166c8403732286181a5689c9e9d0
motschsv <motschsv@nomotschsv.fritz.box>
2017-12-14 20:27:24 +0100

    ODTExport, exemplarischen Gedankengang hinzugefügt.

--- ProPraProjekt1/src/applicationLogic/ODTExport.java
+++ ProPraProjekt1/src/applicationLogic/ODTExport.java
@@ -9,18 +9,44 @@ import org.odftoolkit.simple.TextDocument;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import dataStorageAccess.ResultAccess;
+
 public class ODTExport {
 	private TextDocument odf;
 	private Map<String, String> map = new HashMap<String, String>();
+	static ResultComplete data;
 
 	public static void export() throws Exception {
+
 		File file1 = new File("odt/template.odt");
 		File file2 = new File("odt/result.odt");
+		data = ResultAccess.getCompleteResult(1);
+		
 
 		ODTExport template = new ODTExport();
 		template.readOdt(file1);
-		template.setVariable("TextBox1", "Das ist ein Test");
-		template.setVariable("TextBox2", "Ein weiterer Test");
+		template.setVariable("txtApprovalNr", String.valueOf(data.getId()));
+		template.setVariable("txtCompnay", data.getCompanyPlant().getCompany().getName());
+		template.setVariable("txtCompanyStreet", data.getCompanyPlant().getPlantStreet());
+		template.setVariable("txtCompanyZip", String.valueOf(data.getCompanyPlant().getPlantZip()));
+		template.setVariable("txtCompanyCity", data.getCompanyPlant().getPlantCity());
+		
+		if (data.isFrequencyControlledUtilities()) {
+			template.setVariable("cbFrequencyControlledUtilities0", "Ein weiterer Test");
+			template.setVariable("cbFrequencyControlledUtilities1", "Ein weiterer Test");
+		} else {
+			template.setVariable("cbFrequencyControlledUtilities0", "Ein weiterer Test");
+			template.setVariable("cbFrequencyControlledUtilities1", "Ein weiterer Test");
+		}
+
+		if (data.isPrecautionsDeclared()) {
+			template.setVariable("cbPrecautionsDeclared0", "true");
+			template.setVariable("cbPrecautionsDeclared1", "false");
+		} else {
+			template.setVariable("cbPrecautionsDeclared0", "true");
+			template.setVariable("cbPrecautionsDeclared1", "false");
+		}
+	
 		template.saveOdt(file2);
 	}
 
