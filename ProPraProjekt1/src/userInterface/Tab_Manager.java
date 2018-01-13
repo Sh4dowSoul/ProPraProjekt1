0d7f81ac56833eddeb16d8aa92659d2b68ca5a84
Salih <Salih@Salih-PC.fritz.box>
2018-01-13 19:24:45 +0100

    Manager Tab erstellt

---
+++ ProPraProjekt1/src/userInterface/Tab_Manager.java
@@ -0,0 +1,44 @@
+package userInterface;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Tab_Manager implements Initializable {
+
+//	*** ADD NEW DEFECT ***
+	@FXML private TextField newIdField;
+	@FXML private TextField newDefectField;
+	@FXML private Button discardNewDefectButton;
+	@FXML private Button createNewDefectButton;
+	
+//	*** EDIT A DEFECT ***
+	@FXML private TextField toEditDefectField;
+	@FXML private TextField toEditDefectIdField;
+	@FXML private TextField newEditDefectField;
+	@FXML private TextField newEditDefectIdField;
+	@FXML private Button discardEditButton;
+	@FXML private Button editDefectField;
+	@FXML private Button editDefectIdField;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
